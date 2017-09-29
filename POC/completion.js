4fbd869fb910a068e2a6ee8505b41334ce8a5e40
sharris <sharris@ipn.pt>
2017-09-29 11:45:27 +0100

    Make it harder t (longwinded) to crack for the smart kids

--- POC/completion.js
+++ POC/completion.js
@@ -3,16 +3,23 @@ const prompt = require('prompt')
 const sha1 = require('sha1')
 
 prompt.start()
-
+// Am i overdoing this?
 prompt.get(['username', 'level'], function(err, result) {
 	if(err) throw new Error(err)
-	let sha1text = sha1(result['username'] + result['level'])
-	const getDigits = new RegExp(/\d/, 'g');
-	let str = ""
+	let usernameEncrypt = ""
+	const reId = new RegExp(/(.{4})(.{4})/)
 	for(let i = 0; i < 4; i++) {
-		str += getDigits.exec(sha1text)
+		let match = reId.exec(result['username'])
+		usernameEncrypt += match[1][i] + match[2][i]
 	}
-	console.log(str)
-	return str
+	console.log(usernameEncrypt)
+	let sha1text = sha1(usernameEncrypt + result['level'])
+	let output = ""
+	const getDigits = new RegExp(/\d/, 'g');
+	for(let i = 0; i < 4; i++) {
+		output += getDigits.exec(sha1text)
+	}
+	console.log(output)
+	return output
 })
 // 3935
