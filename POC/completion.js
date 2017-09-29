40d91652490bd25ebb7c8d64781bbfe9293ea3f4
sharris <sharris@ipn.pt>
2017-09-29 11:13:48 +0100

    Get 4-digit pin per oxford username. Eg 7016 for sedm4648 on level 2

--- POC/completion.js
+++ POC/completion.js
@@ -7,5 +7,12 @@ prompt.start()
 prompt.get(['username', 'level'], function(err, result) {
 	if(err) throw new Error(err)
 	let sha1text = sha1(result['username'] + result['level'])
-	console.log(sha1text)
+	const getDigits = new RegExp(/\d/, 'g');
+	let str = ""
+	for(let i = 0; i < 4; i++) {
+		str += getDigits.exec(sha1text)
+	}
+	console.log(str)
+	return str
 })
+// 3935
